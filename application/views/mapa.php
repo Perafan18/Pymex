44d89dae3a3db4d57d98fca581822c52f8ecf32c
Pedro Perafan <pedroperafan18@gmail.com>
2015-08-08 13:28:33 -0500

    Primer Push
    Primer Push del tunahack

---
+++ application/views/mapa.php
@@ -0,0 +1,15 @@
+
+		<div class="col-md-8 well">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59117.33565964228!2d-100.96230610263673!3d22.17040985441824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1439053271256" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+		<div class="col-md-4 well">
+      <div class="list-group">
+        <a href="#" class="list-group-item">
+          Nombre: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At tempore magnam ab odio, tempora sequi minima repudiandae vero quo illum harum dolore natus, doloribus. Delectus id mollitia minima in repellat?
+        </a>
+        <a href="#" class="list-group-item">Giro: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nam quaerat, assumenda alias perspiciatis soluta. Soluta sed quos, dolorum distinctio vitae vero, nostrum sunt dolorem at corporis tempore molestias possimus!</a>
+        <a href="#" class="list-group-item">Colonia: Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, quod impedit. Veniam, repellendus! Laudantium quod omnis natus veniam sunt adipisci temporibus voluptatibus dolore, a optio doloribus excepturi. Deleniti libero, sint!</a>
+      </div>
+    </div>
---
+++ application/views/tabla.php
@@ -0,0 +1,18 @@
+
+        <table class="table  table-bordered">
+          <tr>
+            <td>Nombre</td>
+            <td>Tipo de giro</td>
+            <td>direccion</td>
+            <td>Ver en mapa</td>
+            
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>                       
+              <a class="btn btn-default" href=" " role="button">Link</a>
+            </td>         
+          </tr>
+        </table>
