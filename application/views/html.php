418686484cf43865c6bd29c494152995ec53b569
gerard0310 <lonpe_1450@hotmail.com>
2015-08-08 21:34:48 -0500

    CuartoPatch
    nuevo controlador, mapa con opcion de seleccionar un lugar

--- application/views/html.php
+++ application/views/html.php
@@ -38,6 +38,7 @@
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
 	        <li><a href="<?=$url?>inicio/negociosCercanos">Ver negocios cercanos</a></li>
+	        <li><a href="<?=$url?>inicio/nuevo">mapa</a></li>
 	        <li class="dropdown">
 	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Busqueda Personalizada <span class="caret"></span></a>
 	          <ul class="dropdown-menu">
